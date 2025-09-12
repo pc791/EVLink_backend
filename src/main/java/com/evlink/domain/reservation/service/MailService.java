682f047c98e37762ec6cae6a1bf408825c7e7714
Bok <pc791@naver.com>
2025-09-12 12:58:00 +0900

    import, xml sql 구문 합치기

--- src/main/java/com/evlink/domain/reservation/service/MailService.java
+++ src/main/java/com/evlink/domain/reservation/service/MailService.java
@@ -5,7 +5,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import com.evlink.domain.charger.vo.ChargerVO;
+import com.evlink.domain.mypage.vo.ChargerVO;
 import com.evlink.domain.reservation.vo.ReservationVO;
 
 import jakarta.mail.MessagingException;
